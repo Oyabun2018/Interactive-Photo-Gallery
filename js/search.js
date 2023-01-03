8ba164d287fdd299872a6738c7c66ffed184221c
Oyabun2018 <40612176+Oyabun2018@users.noreply.github.com>
2023-01-03 21:04:58 +0100

    Final version

---
+++ js/search.js
@@ -0,0 +1,6 @@
+
+const pictureSearch = document.getElementById('pictureSearch');
+pictureSearch.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowerCase();
+    let keyword = document.querySelectorAll('a.data-caption')
+});
